7fd302c8e6575ba870999f209c12a6a2e9f63f5a
Tanarys <tiffanydpro923@gmail.com>
2022-10-21 10:17:24 +0200

    maj item repository test

--- MediathequeBackend/src/test/java/com/example/Mediatheque/RepositoryTests/ItemRepositoryTest.java
+++ MediathequeBackend/src/test/java/com/example/Mediatheque/RepositoryTests/ItemRepositoryTest.java
@@ -12,16 +12,21 @@ import com.atos.Mediatheque.repository.ItemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest
+@ContextConfiguration
+@SpringBootTest
 public class ItemRepositoryTest {
 
 	@Autowired
 	ItemRepository itemRepository;
 	
 	@Test
+
 	void testByContainingTitreIgnoringCase() {
-		Optional<Item> found = itemRepository.findBytitreContainingIgnoreCase("Formidable");
+		Optional<Item> found = itemRepository.findBytitreContainingIgnoreCase("Tolkien");
 		assertTrue(found.isPresent());
 		
 		Optional<Item> notFound = itemRepository.findBytitreContainingIgnoreCase("notFound");
