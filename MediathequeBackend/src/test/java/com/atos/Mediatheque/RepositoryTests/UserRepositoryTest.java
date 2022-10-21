40c20e3b1ccdb9e9f24c206b263422bbf7f84efe
Tanarys <tiffanydpro923@gmail.com>
2022-10-21 14:45:29 +0200

    problem 1 on test files fixed

--- MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/UserRepositoryTest.java
+++ MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/UserRepositoryTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.atos.Mediatheque.model.User;
 import com.atos.Mediatheque.repository.UserRepository;
 
+@DataJpaTest
 public class UserRepositoryTest {
 	@Autowired
 	UserRepository userRepository; 
