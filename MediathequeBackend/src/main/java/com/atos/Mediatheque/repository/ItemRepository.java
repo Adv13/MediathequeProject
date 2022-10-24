37a617331571c7440fafd8c461141a68b8929ba2
Tanarys <tiffanydpro923@gmail.com>
2022-10-24 11:11:56 +0200

    maj project

--- MediathequeBackend/src/main/java/com/atos/Mediatheque/repository/ItemRepository.java
+++ MediathequeBackend/src/main/java/com/atos/Mediatheque/repository/ItemRepository.java
@@ -14,9 +14,6 @@ import com.atos.Mediatheque.model.Item;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-	//	Rechercher tous les documents deja prédéfits dans la classe de JPARepository 
-	//	
-	//  Parcourir le catalogue du stock et visualiser les items disponibles à l’emprunt.
 	// ReadALL dispo
 	@Query("select i from Item i where i.nombreExemplaires >0")
 	public List<Item> findItemDisponible();
@@ -39,7 +36,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 	//	public List<Document> findByRecentDateParution(Date date); 
 
 	//afficher les documents dont le titre contenant une chaîne particulière
-	public List<Item> findByTitreContainingIgnoreCase(String chaineTitre);
+	public Optional<Item> findByTitreContainingIgnoreCase(String chaineTitre);
+	public List<Item> findByTitreContainingIgnoreCase2(String chaineTitre);
 	//afficher les documents par date de parution
 	//public Optional<Item> findAllByDateParution(Date dateParution);
 
--- MediathequeBackend/src/test/java/com/atos/Mediatheque/ServiceTests/MediathequeServiceTest.java
+++ MediathequeBackend/src/test/java/com/atos/Mediatheque/ServiceTests/MediathequeServiceTest.java
@@ -1,8 +1,17 @@
 package com.atos.Mediatheque.ServiceTests;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.atos.Mediatheque.model.Emprunt;
+import com.atos.Mediatheque.model.Item;
+import com.atos.Mediatheque.model.User;
 import com.atos.Mediatheque.repository.EmpruntRepository;
 import com.atos.Mediatheque.repository.ItemRepository;
 import com.atos.Mediatheque.service.IMediatheque;
@@ -20,7 +29,23 @@ public class MediathequeServiceTest {
 
 	@Test
 	
-	public void whenAllIsOkEmpruntIsCreated() {
+	public void whenAllIsOkEmpruntIsCreated() throws Exception {
+		
+		User user2 = new User();
+		user2.setId(2l);
+
+		List<Item> docs = new ArrayList<>();
+		Item doc1 = new Item();
+		doc1.setId(1l);
+		Item doc2 = new Item();
+		doc2.setId(2l);
+		docs.add(doc1);
+		docs.add(doc2);
+		
+		Emprunt emprunt = mediathequeService.effectuerEmprunt(user2, docs);
+		
+		assertNotNull(emprunt.getNumero());
+		assertTrue(emprunt.getItems().size() == 2);
 		
 	}
 }
