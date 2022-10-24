99dc4606b3125889aff77bc7dc511cf47818d754
Tanarys <tiffanydpro923@gmail.com>
2022-10-24 10:00:56 +0200

    news emprunt tests

--- MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/EmpruntRepositoryTest.java
+++ MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/EmpruntRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.project.model.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.atos.Mediatheque.model.Copie;
@@ -35,16 +36,33 @@ public class EmpruntRepositoryTest {
 	}
 	
 	@Test
-	void testRestituerItemById() {
-		Emprunt e = empruntService.effectuerEmprunt(1, 1);
-		System.out.println("Restituer item emprunté "+e);
+	void testRestituerItemById() throws Exception {
+		
+		User user = new User();
+		user.setId(1l);
+		List<Item> docs = new ArrayList<>();
+		Item doc1 = new Item();
+		doc1.setId(4l);
+		docs.add(doc1);
+		
+		Emprunt e = empruntService.effectuerEmprunt(user, docs);
+		System.out.println("Restituer item emprunté : "+e);
 			
 	}
 	
 	
     @Test
-    void testFindByUsers(){
+    void testFindByUsers() throws Exception{
+    	
         User user = new User();
+        user.setId(1l);
+		List<Item> docs = new ArrayList<>();
+		Item doc1 = new Item();
+		doc1.setId(4l);
+		docs.add(doc1);
+        
+        
+        Emprunt e = empruntService.effectuerEmprunt(user, docs);
         user.setId(2l);
 
         List<Emprunt> findByUserResult = empruntRepository.findEmpruntById(user);
--- MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/ItemRepositoryTest.java
+++ MediathequeBackend/src/test/java/com/atos/Mediatheque/RepositoryTests/ItemRepositoryTest.java
@@ -65,7 +65,6 @@ public class ItemRepositoryTest {
 		
 	}
 	
-
 	
 	//@Test
 	//void testByContainingDateParution() {
